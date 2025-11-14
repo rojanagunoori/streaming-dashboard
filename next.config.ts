185ebda483ce2e830735fe1700aca62a1ad314f8
rojanagunoori <nagunooriroja@gmail.com>
2025-11-14 10:42:18 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    // TMDB poster base domain
+    domains: ['image.tmdb.org'],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
